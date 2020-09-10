455a0d4b6f82931454ec3129a676ae1d8f62b8bb
haritha-git <harithagit@gmail.com>
2020-09-10 19:10:38 +0530

    reduce parmeters for runtime optimization

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -79,7 +79,7 @@ def build_model():
 
     parameters = {'clf__estimator__criterion': ['entropy'],
               'clf__estimator__max_depth': [10, 50, None],
-              'clf__estimator__min_samples_leaf':[2, 5, 10],
+             # 'clf__estimator__min_samples_leaf':[2, 5, 10],
               'clf__estimator__n_estimators': [100, 200]}
 
     cv=GridSearchCV(pipeline, param_grid=parameters, cv=2, n_jobs=-1, verbose=3)
